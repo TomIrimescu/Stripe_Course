86339e06e10f72d7e4b9f5f85acf028d8c9ebe62
Tom Irimescu <tom@irimescu.com>
2020-01-28 15:57:56 -0500

    Initial set up Node back end & environment variables

---
+++ server/main.ts
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+console.log('Loaded environment config: ', result.parsed);
+
+import { initServer } from './server';
+
+initServer();
